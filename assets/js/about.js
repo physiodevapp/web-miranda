a119ee3f2122bf017e35c9cf42c258f2ae509dea
physiodevapp <edu.gamboa.rodriguez@gmail.com>
2024-07-25 15:46:56 +0200

    style: add styles and facts sections to desktop view

--- assets/js/about.js
+++ assets/js/about.js
@@ -4,6 +4,23 @@ const swiperFacilities = new Swiper(".swiper.about__facilities__slider", {
     el: ".swiper-pagination",
     clickable: true
   },
+  breakpoints: {
+    100: {
+      slidesPerView: 1, 
+      grid: {
+        fill:	'row',
+        rows: 1,
+      },
+    },
+    1000: {
+      spaceBetween: 10,
+      slidesPerView: 3, 
+      grid: {
+        fill:	'row',
+        rows: 2,
+      },
+    }
+  }
 });
 
 const swiperFacts = new Swiper(".swiper.about__facts__slider", {
@@ -11,6 +28,23 @@ const swiperFacts = new Swiper(".swiper.about__facts__slider", {
     el: ".swiper-pagination",
     clickable: true
   },
+  breakpoints: {
+    100: {
+      slidesPerView: 1, 
+      grid: {
+        fill:	'row',
+        rows: 1,
+      },
+    },
+    1000: {
+      spaceBetween: 20,
+      slidesPerView: 2, 
+      grid: {
+        fill:	'row',
+        rows: 1,
+      },
+    }
+  }
 });
 
 const facilitiesSliders = document.querySelectorAll(".about__facilities__slider .swiper-wrapper .facilities__slider__number");
